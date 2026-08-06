497c3328c6fb57104c41e65c8b5cce2d9c4702f8
KimuraRafael <rafaelkimura57@gmail.com>
2026-08-06 01:36:03 -0300

    chore: inicializando o backend e frontend do projeto

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+    return (
+        <main>
+          <h1>Gestão de Processos</h1>
+          <p>Frontend da Prova Web funcionando.</p>
+        </main>
+    )
+  }
+  
+export default App
